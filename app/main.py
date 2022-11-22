1d0a7b8caa4348657db5c3f809d3a693a3e68cdb
budaevdigital <budaev.digital@gmail.com>
2022-11-22 12:16:06 +0500

    Обновил настройки форматера Black

--- app/main.py
+++ app/main.py
@@ -5,9 +5,7 @@ import uvicorn
 # Импортируем настройки проекта
 from core.config import settings
 
-app = FastAPI(title=settings.app_title)
+app = FastAPI(title=settings.app_title, description=settings.app_description)
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "__main__:app", reload=True, env_file=".env", host="0.0.0.0", port=8000
-    )
+    uvicorn.run("__main__:app", reload=True, env_file=".env", host="0.0.0.0", port=8000)
