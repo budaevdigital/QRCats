2f64a820aff3ee05538bda5ba9340ff13b2c2d2d
budaevdigital <budaev.digital@gmail.com>
2022-11-22 12:25:29 +0500

    fix black

--- app/main.py
+++ app/main.py
@@ -8,4 +8,6 @@ from core.config import settings
 app = FastAPI(title=settings.app_title, description=settings.app_description)
 
 if __name__ == "__main__":
-    uvicorn.run("__main__:app", reload=True, env_file=".env", host="0.0.0.0", port=8000)
+    uvicorn.run(
+        "__main__:app", reload=True, env_file=".env", host="0.0.0.0", port=8000
+    )
