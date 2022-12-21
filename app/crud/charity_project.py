0f19de0cca221ae5b8de863ea836afb49c6fb304
budaevdigital <budaev.digital@gmail.com>
2022-12-21 21:09:46 +0500

    fix: Change the name func on correct

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -9,7 +9,7 @@ from app.models import CharityProject
 
 
 class CRUDCharityProject(CRUDBase):
-    async def get_name_by_id(
+    async def get_id_by_name(
         self, charity_name: str, session: AsyncSession
     ) -> Optional[int]:
         db_charity_id = await session.execute(
@@ -19,15 +19,6 @@ class CRUDCharityProject(CRUDBase):
         )
         db_charity_id = db_charity_id.scalars().first()
         return db_charity_id
-
-    async def get_project_by_id(
-        self, charity_id: int, session: AsyncSession
-    ) -> Optional[CharityProject]:
-        db_charity = await session.execute(
-            select(CharityProject).where(CharityProject.id == charity_id)
-        )
-        db_charity = db_charity.scalars().first()
-        return db_charity
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
